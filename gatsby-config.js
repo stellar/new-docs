19ce6fdab976072e2eef4af002b0845fdb2ccd07
Carl Vitullo <carl.vitullo@gmail.com>
2019-04-19 12:25:13 -0400

    Initial project (customized)

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+let contentfulConfig;
+
+try {
+  // Load the Contentful config from the .contentful.json
+  contentfulConfig = require("./.contentful");
+} catch (_) {}
+
+// Overwrite the Contentful config with environment variables if they exist
+contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  accessToken:
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+};
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the delivery token need to be provided.",
+  );
+}
+
+module.exports = {
+  siteMetadata: {
+    title: "Stellar - Develop the world's new financial system",
+  },
+  pathPrefix: "/gatsby-contentful-starter",
+  plugins: [
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
+  ],
+};
