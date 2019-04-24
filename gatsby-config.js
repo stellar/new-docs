fa23b7f7c0fabcb6a397acbcf65e608cbb559525
Carl Vitullo <carl@stellar.org>
2019-04-24 16:06:57 -0400

    Update contentful api key loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,13 @@
-let contentfulConfig;
+require("dotenv").config();
 
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require("./.contentful");
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken:
-    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
 };
 
-const { spaceId, accessToken } = contentfulConfig;
-
-if (!spaceId || !accessToken) {
+if (!contentfulConfig.spaceId || !contentfulConfig.accessToken) {
   throw new Error(
-    "Contentful spaceId and the delivery token need to be provided.",
+    "CONTENTFUL_SPACE_ID and CONTENTFUL_DELIVERY_TOKEN need to be provided in .env.",
   );
 }
 
