75e26fa08da6cc7d5fb664e0eb9f9e2f8ce4e3b8
Jeesun Kim <jeesunikim@users.noreply.github.com>
2019-05-09 11:15:04 -0400

    footer_desktop > icons for the footer added (#9)
    * footer_desktop > icons that are needed for footer added
    * footer_desktop > svg icons moved into its own directory

---
+++ src/basics/Icons.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const ArrowInCircle = styled.button`
+  position: relative;
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  background-color: ${(props) => props.bgColor};
+
+  ::after {
+    content: "\u2192";
+    color: ${(props) => props.arrowColor};
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    line-height: 30px;
+    vertical-align: middle;
+    height: 30px;
+    width: 30px;
+  }
+`;
+
+/**
+ * @desc Arrow in Circle Icon
+ * @param String
+ * Example: The Circle Arrow in Footer's Email Address input
+ */
+export const IconArrowInCircle = ({ bgColor, arrowColor }) => {
+  return <ArrowInCircle bgColor={bgColor} arrowColor={arrowColor} />;
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,20 @@
+/**
+ * @desc calculates the column width in percentage (%)
+ * @param Number { col } - { # of columns we want / a total # of columns }
+ * Example: If there are 6 columns and we want the width of the single column, simply pass { 1/6 }
+ * @return string - width %
+ */
 export const getGridItemWidthPercent = ({ col }) => {
   const percent = col * 100;
   return `${percent}%;`;
+};
+
+/**
+ * @desc get the current full year
+ * * Example: 2019
+ * @return string = full year
+ */
+
+export const getCurrentYear = () => {
+  return new Date().getFullYear();
 };
