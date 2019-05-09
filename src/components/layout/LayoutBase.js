20f6f3d5121701a88fc3aee10fcd17de946fa8e8
Carl Vitullo <vcarl@users.noreply.github.com>
2019-05-09 14:17:06 -0400

    Add favicon and open graph metadata (#11)
    * Add favicon and open graph metadata
    
    * Allow description to be extended per-page

--- src/components/layout/LayoutBase.js
+++ src/components/layout/LayoutBase.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import styled, { ThemeProvider } from "styled-components";
 
+import favicon from "../../assets/favicon/favicon.ico";
+
 import Locale from "../Locale";
 import Navigation, { defaultTheme } from "../Navigation";
 import Footer from "../Footer";
@@ -12,6 +14,7 @@ const El = styled.div``;
 const LayoutBase = ({
   metadata,
   pageContext,
+  description = "",
   children,
   navTheme = defaultTheme,
 }) => {
@@ -23,6 +26,14 @@ const LayoutBase = ({
     >
       <Helmet>
         <title>{metadata.title}</title>
+        <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+        <meta
+          name="description"
+          content={`${metadata.description}${description && ` ${description}`}`}
+        />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metadata.siteUrl} />
       </Helmet>
       <ThemeProvider theme={(theme) => ({ ...theme, ...navTheme })}>
         <Navigation />
@@ -37,7 +48,9 @@ LayoutBase.propTypes = {
   children: PropTypes.node.isRequired,
   metadata: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
   }).isRequired,
+  description: PropTypes.string,
   navTheme: PropTypes.object,
   pageContext: PropTypes.shape({
     locale: PropTypes.string.isRequired,
