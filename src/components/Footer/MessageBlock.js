1f77d1520731bad15433bfff624ef56c3839ef3a
Jeesun Kim <jeesunikim@users.noreply.github.com>
2019-06-19 17:22:04 -0400

    blogpost_polish/dev_letter_url > added a dev newsletter url and text for dev blog post (#67)
    * blogpost_polish/dev_letter_url > added a dev newsletter url and text for dev blog post
    
    * blogpost_polish/dev_letter_url > since <SubscribeBlock/> is being used elsewhere, moved it to /components
    
    * blogpost_polish/dev_letter_url > using Regex to deliever the error message
    
    * Fix import path

--- src/components/Footer/MessageBlock.js
+++ src/components/Footer/MessageBlock.js
@@ -2,7 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Trans } from "@lingui/macro";
 import styled from "styled-components";
+
 import { PALETTE } from "constants/styles";
+import { Link } from "basics/Links";
 
 const ResponseStatusEl = styled.span`
   position: absolute;
@@ -10,21 +12,53 @@ const ResponseStatusEl = styled.span`
   padding-top: 0.625rem;
   font-size: 0.75rem;
 `;
+const LinkEl = styled(Link)`
+  padding-left: 0.2rem;
+  text-decoration: underline;
+`;
+
+const RegexErrorMessage = ({ errorMessage }) => {
+  const linkAttrRegex = /\s*(['"])(https?:\/\/.+?)\1/g;
+  const MESSAGE_URL = errorMessage.match(linkAttrRegex);
+
+  if (MESSAGE_URL) {
+    const firstMessage = errorMessage.split("<a href")[0];
+    const CONVERT_URL_TO_VALID = errorMessage
+      .match(/\s*(['"])(https?:\/\/.+?)\1/g)[0]
+      .replace(/"/g, "");
+
+    return (
+      <>
+        <Trans>{firstMessage} </Trans>
+        <LinkEl newTab href={CONVERT_URL_TO_VALID}>
+          <Trans>Click here to update your profile.</Trans>
+        </LinkEl>
+      </>
+    );
+  }
+  return <Trans>errorMessage</Trans>;
+};
+
+RegexErrorMessage.propTypes = {
+  errorMessage: PropTypes.string.isRequired,
+};
 
 const MessageBlock = (props) => {
   const { response, email } = props;
   const ERROR_MESSAGE =
-    response.message.split(" ")[0] === "0"
-      ? "Please type a valid email address"
-      : response.message;
+    response.message.split(" ")[0] === "0" ? (
+      "Please type a valid email address"
+    ) : (
+      <RegexErrorMessage errorMessage={response.message} />
+    );
 
   return response.status === "error" ? (
-    <ResponseStatusEl isError>
-      <Trans>{ERROR_MESSAGE}</Trans>
-    </ResponseStatusEl>
+    <ResponseStatusEl isError>{ERROR_MESSAGE}</ResponseStatusEl>
   ) : (
     <ResponseStatusEl>
-      <Trans>Confirmation email has been sent to {email}</Trans>
+      <Trans>
+        Confirmation email has been sent to <strong>{email}</strong>
+      </Trans>
     </ResponseStatusEl>
   );
 };
