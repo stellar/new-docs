84f5211207c9a5ef4f574a29aabc8eda4f111739
Carl Vitullo <carl@stellar.org>
2020-03-09 18:22:10 -0400

    Remove unused style constants

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -10,29 +10,15 @@ export const PALETTE = {
   black90: "#292d3d",
   light: "#fbfaf7",
   lightestGrey: "#cccccc",
-  notQuiteLightestGray: "#d8d8d8",
-  lighterGrey: "#f4f4f3",
   lightGrey: "#999999",
   mediumGrey: "#979797",
   darkGrey: "#4a4a49",
-  lightBlack: "#1e1e1e",
   dark: "#000000",
-  lighterYellow: "#fae7cb",
-  lightYellow: "#ffed00",
   orange: "#ff5500",
   yellow: "#ffa51e",
   green: "#00aa46",
-  teal: "#21b0a4",
-  coolBlue: "#39abd4",
-  darkCoolBlue: "#0c0d71",
-  lightestCoolPurple: "#b3a6ed",
-  lighterCoolPurple: "#af80f8",
-  lightCoolPurple: "#7b35f7",
-  coolPurple: "#501e83",
-  lightPurple: "#975581",
   purple: "#490be3",
   purpleBlue: "#3e1bdb",
-  coolPink: "#97557f",
   lightGreen: "#abcc7d",
   lightOrage: "#e5c687",
 };
@@ -96,8 +82,6 @@ export const MEDIA_QUERIES = {
 };
 
 export const Z_INDEXES = {
-  watermarkBase: -10,
-  watermark: -5,
   body: 0,
   nav: 10,
   modal: 20,
@@ -119,82 +103,11 @@ export const CSS_TRANSITION_DELAY = {
   default: "50ms",
 };
 
-export const NAV_HEIGHT = 80;
-export const NAV_HEIGHT_WITH_BANNER = 120;
-
 export const DEFAULT_COLUMN_WIDTH = {
   leftColumn: 15,
   rightColumn: 18,
   gridArea: {
     gtDesktop: 3.5,
     ltDesktop: 2.5,
-  },
-};
-
-export const NAV_THEMES = {
-  default: {
-    background: THEME.body,
-    border: THEME.border,
-    link: tinycolor(THEME.text)
-      .setAlpha(0.7)
-      .toRgbString(),
-    cta: {
-      body: PALETTE.yellow,
-      text: PALETTE.light,
-    },
-    logo: PALETTE.dark,
-  },
-  dark: {
-    background: PALETTE.dark,
-    border: PALETTE.darkGrey,
-    link: tinycolor(PALETTE.light)
-      .setAlpha(0.8)
-      .toRgbString(),
-    cta: {
-      body: PALETTE.purple,
-      text: PALETTE.light,
-    },
-    logo: PALETTE.light,
-  },
-  docs: {
-    background: PALETTE.white,
-    border: REDESIGN_PALETTE.grey[1],
-    link: tinycolor(PALETTE.light)
-      .setAlpha(0.8)
-      .toRgbString(),
-    cta: {
-      body: PALETTE.purple,
-      text: PALETTE.light,
-    },
-    logo: PALETTE.dark,
-  },
-};
-
-export const SUBPAGE_THEMES = {
-  default: {
-    background: PALETTE.light,
-    fgColor: PALETTE.dark,
-    categoryColor: PALETTE.purple,
-  },
-  yellow: {
-    background: PALETTE.yellow,
-    fgColor: PALETTE.light,
-    categoryColor: PALETTE.lightYellow,
-  },
-  dark: {
-    background: PALETTE.dark,
-    fgColor: PALETTE.light,
-    categoryColor: PALETTE.purple,
-  },
-  lumens: {
-    background: "", // Brought in on lumen page as an optimization
-    fgColor: PALETTE.light,
-    categoryColor: PALETTE.lightPurple,
-  },
-  bland: {
-    background: PALETTE.light,
-    fgColor: PALETTE.dark,
-    categoryColor: PALETTE.notQuiteLightestGray,
-    isTextFilled: true,
   },
 };
