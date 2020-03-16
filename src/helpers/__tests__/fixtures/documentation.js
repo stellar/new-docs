9a35984a166dc2bbf14dd0a03ee8ed64033999c9
Carl Vitullo <carl@stellar.org>
2020-03-16 18:07:35 -0400

    Add a simple snapshot test to buildDocsContents

---
+++ src/helpers/__tests__/fixtures/documentation.js
@@ -0,0 +1,430 @@
+export const input = {
+  basic: [
+    {
+      fieldValue: "docs",
+      nodes: [
+        {
+          id: "64110fd0-8353-5cdb-aad9-d58e8df5f73c",
+          modifiedTime: "Mar. 16, 2020",
+          name: "index",
+          relativePath: "docs/index.mdx",
+          childMdx: {
+            id: "3649032a-87b2-5c22-a902-7b7232dfe842",
+            frontmatter: {
+              title: "Docs Specimen Page",
+              description:
+                "The docs specimen page. This is a dummy description so that the `description` property gets added to the graphQL schema",
+              order: -100,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 0,
+                title: "Documentation",
+              },
+            },
+          },
+        },
+      ],
+    },
+    {
+      fieldValue: "docs/building-apps",
+      nodes: [
+        {
+          id: "36a16bb4-b974-5ef2-974b-679314c21681",
+          modifiedTime: "Mar. 04, 2020",
+          name: "index",
+          relativePath: "docs/building-apps/index.mdx",
+          childMdx: {
+            id: "9f9f92e5-4ed6-5e35-a911-c179c411a3bd",
+            frontmatter: {
+              title: "Overview",
+              description: null,
+              order: 0,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 40,
+                title: "Building Apps",
+              },
+            },
+          },
+        },
+        {
+          id: "b15eb4f4-ccc3-5990-b7cc-8ff3c010cf6e",
+          modifiedTime: "Mar. 04, 2020",
+          name: "project-setup",
+          relativePath: "docs/building-apps/project-setup.mdx",
+          childMdx: {
+            id: "01697ea8-b028-558c-854e-4eaf1ebc56b5",
+            frontmatter: {
+              title: "Project Setup",
+              description: null,
+              order: 20,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 40,
+                title: "Building Apps",
+              },
+            },
+          },
+        },
+      ],
+    },
+    {
+      fieldValue: "docs/building-apps/connect-to-anchors",
+      nodes: [
+        {
+          id: "64ae1b5d-31fe-52c7-baba-7a33c37d1cce",
+          modifiedTime: "Mar. 16, 2020",
+          name: "index",
+          relativePath: "docs/building-apps/connect-to-anchors/index.mdx",
+          childMdx: {
+            id: "a197a16d-2726-56bb-9b2c-21c0803aede6",
+            frontmatter: {
+              title: "Intro",
+              description: null,
+              order: 0,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 60,
+                title: "Connect to anchors",
+              },
+            },
+          },
+        },
+        {
+          id: "83777ec2-5d3e-5a58-908f-f3d28ca730cf",
+          modifiedTime: "Mar. 04, 2020",
+          name: "setup-for-anchored-assets",
+          relativePath:
+            "docs/building-apps/connect-to-anchors/setup-for-anchored-assets.mdx",
+          childMdx: {
+            id: "7c30fffb-dc0f-538c-b441-fab1950397c0",
+            frontmatter: {
+              title: "Setup for anchored assets",
+              description: null,
+              order: 20,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 60,
+                title: "Connect to anchors",
+              },
+            },
+          },
+        },
+        {
+          id: "36b58dbb-c9d9-5f50-adfe-494fb55242b5",
+          modifiedTime: "Mar. 16, 2020",
+          name: "deposit-anchored-assets",
+          relativePath:
+            "docs/building-apps/connect-to-anchors/deposit-anchored-assets.mdx",
+          childMdx: {
+            id: "1d980696-8c4a-5e31-82bf-56668efa8814",
+            frontmatter: {
+              title: "Deposit anchored assets",
+              description: null,
+              order: 30,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 60,
+                title: "Connect to anchors",
+              },
+            },
+          },
+        },
+        {
+          id: "d0f594b9-f81b-5597-bf5f-dd619cb6c735",
+          modifiedTime: "Mar. 04, 2020",
+          name: "withdraw-anchored-assets",
+          relativePath:
+            "docs/building-apps/connect-to-anchors/withdraw-anchored-assets.mdx",
+          childMdx: {
+            id: "295a07f2-aa76-5d59-8743-d5c36fbdbc61",
+            frontmatter: {
+              title: "Withdraw anchored assets",
+              description: null,
+              order: 40,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 60,
+                title: "Connect to anchors",
+              },
+            },
+          },
+        },
+      ],
+    },
+    {
+      fieldValue: "docs/building-apps/create-stellar-wallet",
+      nodes: [
+        {
+          id: "c9c3bed8-9336-5977-84c6-1e2dadd588dd",
+          modifiedTime: "Mar. 09, 2020",
+          name: "index",
+          relativePath: "docs/building-apps/create-stellar-wallet/index.mdx",
+          childMdx: {
+            id: "5d820c7d-12cd-5668-b5f5-87b8e058fd7e",
+            frontmatter: {
+              title: "Overview",
+              description: null,
+              order: 10,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 30,
+                title: "Create a Stellar wallet",
+              },
+            },
+          },
+        },
+        {
+          id: "20ef9be5-3937-56b7-8411-3c221ddd9d1b",
+          modifiedTime: "Mar. 09, 2020",
+          name: "basic-wallet",
+          relativePath:
+            "docs/building-apps/create-stellar-wallet/basic-wallet.mdx",
+          childMdx: {
+            id: "eeddec60-bc80-5094-81a7-28c1922264f6",
+            frontmatter: {
+              title: "Create a Basic Wallet",
+              description: null,
+              order: 20,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 30,
+                title: "Create a Stellar wallet",
+              },
+            },
+          },
+        },
+      ],
+    },
+    {
+      fieldValue: "docs/building-apps/make-use-of-trustlines",
+      nodes: [
+        {
+          id: "9a21bc41-381e-537c-a4f2-45eb9f9567ff",
+          modifiedTime: "Mar. 09, 2020",
+          name: "index",
+          relativePath: "docs/building-apps/make-use-of-trustlines/index.mdx",
+          childMdx: {
+            id: "131a4bd4-5225-549f-aebe-b582e1219e6f",
+            frontmatter: {
+              title: "Overview",
+              description: null,
+              order: 0,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 50,
+                title: "Handle Custom Assets",
+              },
+            },
+          },
+        },
+        {
+          id: "f21dfb1f-7e0a-544a-8d6d-a2652779fdad",
+          modifiedTime: "Mar. 09, 2020",
+          name: "add-trustline-support",
+          relativePath:
+            "docs/building-apps/make-use-of-trustlines/add-trustline-support.mdx",
+          childMdx: {
+            id: "9f3adfd6-f06a-513d-aea1-506ef0d8e50c",
+            frontmatter: {
+              title: "Handle Custom Assets",
+              description: null,
+              order: 20,
+            },
+          },
+          fields: {
+            metadata: {
+              data: {
+                order: 50,
+                title: "Handle Custom Assets",
+              },
+            },
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export const output = {
+  basic: {
+    "/": {
+      articles: {
+        index: {
+          id: "3649032a-87b2-5c22-a902-7b7232dfe842",
+          githubLink:
+            "https://github.com/stellar/new-docs/blob/master/content/docs/index.mdx",
+          modifiedTime: "Mar. 16, 2020",
+          title: "Docs Specimen Page",
+          description:
+            "The docs specimen page. This is a dummy description so that the `description` property gets added to the graphQL schema",
+          url: "/docs/",
+          nextUp: {
+            title: "Overview",
+            url: "/docs/building-apps/create-stellar-wallet/",
+          },
+        },
+      },
+      id: "64110fd0-8353-5cdb-aad9-d58e8df5f73c",
+      topicPath: "/",
+      title: "Documentation",
+    },
+    "/building-apps": {
+      articles: {
+        "/create-stellar-wallet": {
+          articles: {
+            index: {
+              id: "5d820c7d-12cd-5668-b5f5-87b8e058fd7e",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/create-stellar-wallet/index.mdx",
+              modifiedTime: "Mar. 09, 2020",
+              title: "Overview",
+              description: null,
+              url: "/docs/building-apps/create-stellar-wallet/",
+              nextUp: {
+                title: "Create a Basic Wallet",
+                url: "/docs/building-apps/create-stellar-wallet/basic-wallet/",
+              },
+            },
+            "basic-wallet": {
+              id: "eeddec60-bc80-5094-81a7-28c1922264f6",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/create-stellar-wallet/basic-wallet.mdx",
+              modifiedTime: "Mar. 09, 2020",
+              title: "Create a Basic Wallet",
+              description: null,
+              url: "/docs/building-apps/create-stellar-wallet/basic-wallet/",
+              nextUp: null,
+            },
+          },
+          id: "c9c3bed8-9336-5977-84c6-1e2dadd588dd",
+          topicPath: "/building-apps/create-stellar-wallet",
+          title: "Create a Stellar wallet",
+        },
+        index: {
+          id: "9f9f92e5-4ed6-5e35-a911-c179c411a3bd",
+          githubLink:
+            "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/index.mdx",
+          modifiedTime: "Mar. 04, 2020",
+          title: "Overview",
+          description: null,
+          url: "/docs/building-apps/",
+        },
+        "project-setup": {
+          id: "01697ea8-b028-558c-854e-4eaf1ebc56b5",
+          githubLink:
+            "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/project-setup.mdx",
+          modifiedTime: "Mar. 04, 2020",
+          title: "Project Setup",
+          description: null,
+          url: "/docs/building-apps/project-setup/",
+        },
+        "/make-use-of-trustlines": {
+          articles: {
+            index: {
+              id: "131a4bd4-5225-549f-aebe-b582e1219e6f",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/make-use-of-trustlines/index.mdx",
+              modifiedTime: "Mar. 09, 2020",
+              title: "Overview",
+              description: null,
+              url: "/docs/building-apps/make-use-of-trustlines/",
+            },
+            "add-trustline-support": {
+              id: "9f3adfd6-f06a-513d-aea1-506ef0d8e50c",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/make-use-of-trustlines/add-trustline-support.mdx",
+              modifiedTime: "Mar. 09, 2020",
+              title: "Handle Custom Assets",
+              description: null,
+              url:
+                "/docs/building-apps/make-use-of-trustlines/add-trustline-support/",
+            },
+          },
+          id: "9a21bc41-381e-537c-a4f2-45eb9f9567ff",
+          topicPath: "/building-apps/make-use-of-trustlines",
+          title: "Handle Custom Assets",
+        },
+        "/connect-to-anchors": {
+          articles: {
+            index: {
+              id: "a197a16d-2726-56bb-9b2c-21c0803aede6",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/connect-to-anchors/index.mdx",
+              modifiedTime: "Mar. 16, 2020",
+              title: "Intro",
+              description: null,
+              url: "/docs/building-apps/connect-to-anchors/",
+            },
+            "setup-for-anchored-assets": {
+              id: "7c30fffb-dc0f-538c-b441-fab1950397c0",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/connect-to-anchors/setup-for-anchored-assets.mdx",
+              modifiedTime: "Mar. 04, 2020",
+              title: "Setup for anchored assets",
+              description: null,
+              url:
+                "/docs/building-apps/connect-to-anchors/setup-for-anchored-assets/",
+            },
+            "deposit-anchored-assets": {
+              id: "1d980696-8c4a-5e31-82bf-56668efa8814",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/connect-to-anchors/deposit-anchored-assets.mdx",
+              modifiedTime: "Mar. 16, 2020",
+              title: "Deposit anchored assets",
+              description: null,
+              url:
+                "/docs/building-apps/connect-to-anchors/deposit-anchored-assets/",
+            },
+            "withdraw-anchored-assets": {
+              id: "295a07f2-aa76-5d59-8743-d5c36fbdbc61",
+              githubLink:
+                "https://github.com/stellar/new-docs/blob/master/content/docs/building-apps/connect-to-anchors/withdraw-anchored-assets.mdx",
+              modifiedTime: "Mar. 04, 2020",
+              title: "Withdraw anchored assets",
+              description: null,
+              url:
+                "/docs/building-apps/connect-to-anchors/withdraw-anchored-assets/",
+            },
+          },
+          id: "64ae1b5d-31fe-52c7-baba-7a33c37d1cce",
+          topicPath: "/building-apps/connect-to-anchors",
+          title: "Connect to anchors",
+        },
+      },
+      id: "36a16bb4-b974-5ef2-974b-679314c21681",
+      topicPath: "/building-apps",
+      title: "Building Apps",
+    },
+  },
+};
