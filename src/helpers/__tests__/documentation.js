3f1da3ae0b322d9601d7f7c351f75f004f247685
Carl Vitullo <carl@stellar.org>
2020-03-30 18:45:15 -0400

    Add simple tests

--- src/helpers/__tests__/documentation.js
+++ src/helpers/__tests__/documentation.js
@@ -1,8 +1,22 @@
-import { buildDocsContents } from "../documentation";
-import { input, output } from "./fixtures/documentation";
+import { buildDocsContents, buildAttributesList } from "../documentation";
+import { input, output, attributes } from "./fixtures/documentation";
 
 describe("buildDocsContents", () => {
   it("matches snapshot", () => {
     expect(buildDocsContents(input.basic, "docs")).toStrictEqual(output.basic);
+  });
+});
+
+describe("buildAttributesList", () => {
+  it("Handles empty type fields", () => {
+    expect(buildAttributesList(attributes.deeplyNested)).toMatchSnapshot();
+  });
+  it("Throws with missing whitespace", () => {
+    expect(() =>
+      buildAttributesList(attributes.throwOnMissingWhitespace),
+    ).toThrow("No description found");
+  });
+  it("parses _links correctly", () => {
+    expect(buildAttributesList(attributes.weirdString)).toMatchSnapshot();
   });
 });
