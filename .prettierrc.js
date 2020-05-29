851562e3ea1e2f096bfedffc5dfb41d9b681abc5
Jeesun Kim <jeesunikim@users.noreply.github.com>
2020-05-29 11:41:40 -0400

    Update prettier for .mdx (#148)
    * remove prettier config from package.json since this overwrites our custom config
    
    * /api prettified without proseWrap
    
    * /docs prettified without proseWrap

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,5 +2,12 @@ module.exports = {
   ...require("@stellar/prettier-config"),
   // This is mostly content, and prose wrap has a way of exploding markdown
   // diffs. Override the default for a better experience.
-  proseWrap: "never",
+  overrides: [
+    {
+      files: "*.mdx",
+      options: {
+        proseWrap: "never",
+      },
+    },
+  ],
 };
